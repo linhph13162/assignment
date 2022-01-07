eb5fb94d34da616146da456eafa185cbd18175c1
nguyenvanlinhuser <xnguyenvanlinh@gmail.com>
2022-01-07 13:32:01 +0700

    update menu

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const listMenu = ["Trang chủ", "Tuyển sinh ", "Chương trình đào tạo", "Góc sinh viên", "Tuyển dụng"];
+const listMenu = ["Trang chủ", "Tuyển sinh đi ", "Chương trình đào tạo", "Góc sinh viên", "Tuyển dụng"];
 const news = [{
     title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iure, labore dignissimos, quos maxime non voluptatibus minima nisi illo autem enim atque qui consequuntur eos esse libero deserunt porro dicta.",
